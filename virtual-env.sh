e03a2e36387a41a34893705efa5878af377756dc
ta-yuhyeon <codesaturday@gmail.com>
2025-01-03 22:54:00 +0100

    added more commands to virtual-env.sh

--- virtual-env.sh
+++ virtual-env.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 pip install --upgrade pip 
 pip3 install --upgrade pip
+
 pip install virtualenv 
 pip3 install virtualenv
 
-virtualenv 
+virtualenv ansible
+source ./ansible/bin/activate
+deactivate
