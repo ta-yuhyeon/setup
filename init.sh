a75a60d09a65346e290bc648c65710a631b8502f
Ralph.Sabado <77937738+ta-yuhyeon@users.noreply.github.com>
2025-01-07 17:48:05 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 pip3 install -r ./requirements/requirements.txt -q
 git submodule update --init
+export PATH="$PATH:/opt/homebrew/bin:/Users/$(whoami)/Library/Python/3.9/bin"
 ansible-galaxy install -r ./requirements/requirements.yml 
 ansible-playbook setup.yml --ask-become-pass
