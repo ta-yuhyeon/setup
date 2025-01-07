3df152aaf73d476e80c629220d1540f24ce0ab59
Ralph.Sabado <77937738+ta-yuhyeon@users.noreply.github.com>
2025-01-07 16:00:19 +0000

    fix

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 pip3 install -r ./requirements/requirements.txt -q
 export PATH=$PATH:/Users/$(whoami)/Library/Python/3.9/bin
+git submodule update --init
 ansible-galaxy install -r ./requirements/requirements.yml 
 ansible-playbook setup.yml --ask-become-pass
