5827500e928aea8e6151b32019a4698dd9885b94
ta-yuhyeon <codesaturday@gmail.com>
2025-01-07 16:27:14 +0100

    trying to add init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@
 pip3 install -r ./requirements/requirements.txt
 export PATH=$PATH:/Users/$(whoami)/Library/Python/3.9/bin
 ansible-galaxy install -r ./requirements/requirements.yml
+ansible-playbook setup.yml --ask-become-pass
