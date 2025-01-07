5bb44401537bfc06213897c270129f80502e8b16
Ralph.Sabado <77937738+ta-yuhyeon@users.noreply.github.com>
2025-01-07 15:59:33 +0000

    changes

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip3 install -r ./requirements/requirements.txt
+pip3 install -r ./requirements/requirements.txt -q
 export PATH=$PATH:/Users/$(whoami)/Library/Python/3.9/bin
-ansible-galaxy install -r ./requirements/requirements.yml
+ansible-galaxy install -r ./requirements/requirements.yml 
 ansible-playbook setup.yml --ask-become-pass
