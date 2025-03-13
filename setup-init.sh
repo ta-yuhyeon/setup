56ccc6dbd72faf138c6a19d988aff5d97b14e47f
ta-yuhyeon <codesaturday@gmail.com>
2025-03-13 22:49:13 +0100

    changed things

--- setup-init.sh
+++ setup-init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 MAC_OS_BANNER="[SETUP INSTALLATION FOR MACOS] \n"
+GITHUB_SETUP_URL="https://github.com/ta-yuhyeon/setup"
 RAW_GITHUB_SETUP_URL="https://raw.githubusercontent.com/ta-yuhyeon/setup/refs/heads/devel"
 PY_REQUIREMENTS_PATH="/requirements/requirements.txt"
 ANSIBLE_REQUIREMENTS_PATH="/requirements/requirements.yml"
@@ -9,9 +10,8 @@ then
     echo -e $MAC_OS_BANNER
     xcode-select --install
     pip3 install -r <(curl -s $RAW_GITHUB_SETUP_URL$PY_REQUIREMENTS_PATH) -q
-    # git submodule update --init
     export PATH="$PATH:/opt/homebrew/bin:/Users/$(whoami)/Library/Python/3.9/bin"
     curl -s "${RAW_GITHUB_SETUP_URL}${ANSIBLE_REQUIREMENTS_PATH}" -o /tmp/requirements.yml
     ansible-galaxy install -r /tmp/requirements.yml; rm /tmp/requirements.yml
-    ansible-playbook setup.yml --ask-become-pass
+    ansible-pull -U $GITHUB_SETUP_URL -C devel --ask-become-pass
 fi
