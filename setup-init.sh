9cd22e2b5d53c3a49246074df039f9e8ca8103ca
ta-yuhyeon <codesaturday@gmail.com>
2025-03-13 22:22:35 +0100

    refactor in progress

--- setup-init.sh
+++ setup-init.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
-MAC-OS-BANNER="SETUP INSTALLATION FOR MACOS"
+MAC_OS_BANNER="[SETUP INSTALLATION FOR MACOS] \n"
+RAW_GITHUB_SETUP_URL="https://raw.githubusercontent.com/ta-yuhyeon/setup/refs/heads/devel"
+PY_REQUIREMENTS_PATH="/requirements/requirements.txt"
+ANSIBLE_REQUIREMENTS_PATH="/requirements/requirements.yml"
+ANSIBLE_PLAYBOOK_PATH="/setup.yml"
 if [[ -f /usr/bin/sw_vers ]];
 then
-    echo ${MAC-OS-BANNER}
+    echo -e $MAC_OS_BANNER
     xcode-select --install
-    pip3 install -r ./requirements/requirements.txt -q
-    git submodule update --init
+    pip3 install -r <(curl -s $RAW_GITHUB_SETUP_URL$PY_REQUIREMENTS_PATH) -q
+    # git submodule update --init
     export PATH="$PATH:/opt/homebrew/bin:/Users/$(whoami)/Library/Python/3.9/bin"
-    ansible-galaxy install -r ./requirements/requirements.yml 
+    curl -s "${RAW_GITHUB_SETUP_URL}${ANSIBLE_REQUIREMENTS_PATH}" -o /tmp/requirements.yml
+    ansible-galaxy install -r /tmp/requirements.yml; rm /tmp/requirements.yml
     ansible-playbook setup.yml --ask-become-pass
 fi
